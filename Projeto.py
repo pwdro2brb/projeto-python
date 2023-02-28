a1309caf818b36a72c55b74ea45ef11a47e07e25
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-02-27 22:48:17 -0300

    Projeto 1 conversor parte 3 -- tira print do input ao apertar um o primeiro botão

--- Projeto.py
+++ Projeto.py
@@ -1,10 +1,10 @@
 import PySimpleGUI as sg
 
 layout = [
-    [sg.Text('Texto'),sg.Spin(['Item 1', 'Item 2'])],
-    [sg.Button('Botão')],
-    [sg.Input()],
-    [sg.Text('Teste'), sg.Button('Apenas para teste')]
+    [sg.Text('Texto', enable_events= True, key = '-Text-'),sg.Spin(['Item 1', 'Item 2'])],
+    [sg.Button('Botão', key= 'Button1')],
+    [sg.Input(key = ('-Input-'))],
+    [sg.Text('Teste'), sg.Button('Apenas para teste', key = 'Button2')]
 ]
 
 Janela = sg.Window('converter', layout)
@@ -14,4 +14,12 @@ while True:
 
   if event == sg.WIN_CLOSED:
     break
+  if event == 'Button1':
+    print(values['-Input-'])
+  if event == 'Button2':
+    print('Você apertou outro botão')
+  if event == '-Text-':
+    print('Você apertou texto')
+
+  
 Janela.close()
