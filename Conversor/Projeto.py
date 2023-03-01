adbccd72bbcf7148d11a552b425b183a3d50e2bf
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-02-28 22:34:30 -0300

    Projeto 1 conversor Parte 4 -- lógica principal do conversor

--- Conversor/Projeto.py
+++ Conversor/Projeto.py
@@ -1,10 +1,13 @@
 import PySimpleGUI as sg
 
 layout = [
-    [sg.Text('Texto', enable_events= True, key = '-Text-'),sg.Spin(['Item 1', 'Item 2'])],
-    [sg.Button('Botão', key= 'Button1')],
-    [sg.Input(key = ('-Input-'))],
-    [sg.Text('Teste'), sg.Button('Apenas para teste', key = 'Button2')]
+    [
+        sg.Input(key='-Input-'),
+        sg.Spin(['Km para milhas', 'kg para g', 'segundo para minuto'], key='-Unidades-'),
+        sg.Button('Converter', key='-Converter-')
+    ],
+
+    [sg.Text('Saída', key='-saida-')]
 ]
 
 Janela = sg.Window('converter', layout)
@@ -14,15 +17,17 @@ while True:
 
   if event == sg.WIN_CLOSED:
     break
-  if event == 'Button1':
-   Janela['-Text-'].update(values['-Input-'])
 
-  if event == 'Button2':
-    print('Você apertou outro botão')
-    Janela['Button2'].update(visible = False)
-    
-  if event == '-Text-':
-    print('Você apertou texto')
+  if event == '-Converter-':
+    input_value = values['-Input-']
+    if input_value.isnumeric():
+      input_value = float(input_value)
+      match values['-Unidades-']:
+        case 'Km para milhas':
+          output = input_value * 0.6214
+          output_string = f'{input_value} Km é {output} milhas.'
 
-  
+      Janela['-saida-'].update(output_string)
+
+
 Janela.close()
