74278bb1b41b7d14e31adbaabfea552b48dd6bd7
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-03-02 23:33:21 -0300

    Projeto 3 - calculadora  Parte 2-- começo da estilização e prenchimento dos números

--- Calculadora/Calculadora em python.py
+++ Calculadora/Calculadora em python.py
@@ -1,6 +1,18 @@
 import PySimpleGUI as sg
 
-layout = [[sg.Button('=')]]
+sg.theme('DarkGrey6')
+sg.set_options(font = 'Franklin 18', button_element_size=(3,3))
+button_size =(6,3)
+
+layout = [
+    [sg.Text('Saída')],
+    [sg.Button('Limpar', size = button_size), sg.Button('Enter', size = button_size)],
+    [sg.Button('7', size = button_size), sg.Button('8', size = button_size),sg.Button('9', size = button_size),sg.Button('*', size = button_size)],
+    [sg.Button('4', size = button_size), sg.Button('5', size = button_size),sg.Button('6', size = button_size),sg.Button('/', size = button_size)],
+    [sg.Button('1', size = button_size), sg.Button('2', size = button_size),sg.Button('3', size = button_size),sg.Button('-', size = button_size)],
+    [sg.Button('0', size = button_size), sg.Button('.', size = button_size),sg.Button('+', size = button_size)]
+    ]
+
 
 Janela = sg.Window('Calculadora', layout)
 
