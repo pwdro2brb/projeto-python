1858ce3ffc55dff0476c7873e812193ae68d9383
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-03-02 23:45:07 -0300

    Projeto 3 - calculadora Parte 3-- Finalização do layout

--- Calculadora/Calculadora em python.py
+++ Calculadora/Calculadora em python.py
@@ -5,12 +5,12 @@ sg.set_options(font = 'Franklin 18', button_element_size=(3,3))
 button_size =(6,3)
 
 layout = [
-    [sg.Text('Saída')],
-    [sg.Button('Limpar', size = button_size), sg.Button('Enter', size = button_size)],
+    [sg.Text('Saída', font='Franklin 26', justification = 'right', expand_x = True, pad = (10,20))],
+    [sg.Button('Limpar',expand_x=True), sg.Button('Enter', expand_x=True)],
     [sg.Button('7', size = button_size), sg.Button('8', size = button_size),sg.Button('9', size = button_size),sg.Button('*', size = button_size)],
     [sg.Button('4', size = button_size), sg.Button('5', size = button_size),sg.Button('6', size = button_size),sg.Button('/', size = button_size)],
     [sg.Button('1', size = button_size), sg.Button('2', size = button_size),sg.Button('3', size = button_size),sg.Button('-', size = button_size)],
-    [sg.Button('0', size = button_size), sg.Button('.', size = button_size),sg.Button('+', size = button_size)]
+    [sg.Button('0', expand_x=True), sg.Button('.', size = button_size),sg.Button('+', size = button_size)]
     ]
 
 
