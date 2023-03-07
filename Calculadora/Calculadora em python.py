84cb1847057779fdaba5ca21ae8bbfda02e10ef1
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-03-06 22:48:55 -0300

    Projeto 3 - calculadora Parte 6 - Muda o valor na calculadora sem realizar cáculos

--- Calculadora/Calculadora em python.py
+++ Calculadora/Calculadora em python.py
@@ -7,12 +7,13 @@ def create_window(theme):
 
     layout = [
         [sg.Text('Saída',
-                  font='Franklin 26', 
-                  justification = 'right',
-                    expand_x = True, 
-                    pad = (10,20),
-                    right_click_menu = theme_menu)
-        ],
+         font='Franklin 26', 
+         justification='right',
+         expand_x=True, 
+         pad=(10, 20),
+         key='-Text-',
+         right_click_menu=theme_menu)
+    ],
         [sg.Button('Limpar',expand_x=True), sg.Button('Enter', expand_x=True)],
         [sg.Button('7', size = button_size), sg.Button('8', size = button_size),sg.Button('9', size = button_size),sg.Button('*', size = button_size)],
         [sg.Button('4', size = button_size), sg.Button('5', size = button_size),sg.Button('6', size = button_size),sg.Button('/', size = button_size)],
@@ -27,6 +28,7 @@ theme_menu = ['menu', ['LightBrown9','LightBrown9','LightBrown4','random']]
 Janela = create_window('dark')
 
 current_num = []
+full_operation = []
 
 while True:
     event, values = Janela.read()
@@ -40,10 +42,13 @@ while True:
     if event in ['0','1','2','3','4','5','6','7','8','9','.']:
         current_num.append(event)
         num_string = ''.join(current_num)
-        print(event)
+        Janela['-Text-'].update(num_string)
 
     if event in ['/','-','+','*']:
-        print(event)
+        full_operation.append(''.join(current_num))
+        current_num = []
+        full_operation.append(event)
+        print(full_operation)
 
     if event == 'Enter':
         print(event)
