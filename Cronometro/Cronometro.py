963e0ee1d299831fc9d92ec1b0b1f108ec2ee23b
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-03-06 23:16:49 -0300

    Projeto 4 - cronometro  Parte 2 -- Começo da estilização

--- Cronometro/Cronometro.py
+++ Cronometro/Cronometro.py
@@ -1,14 +1,23 @@
 import PySimpleGUI as sg
 
-layout = [[sg.Text('Começo'),
-           sg.Button('cronometragem vai vir', key = '-Começo-')
-           ]]
+sg.theme('random')
 
-Janela = sg.Window('Cronometro', layout)
+layout = [
+    [sg.VPush()],
+    [sg.Text('Tempo')],
+    [sg.Button('Começar', key = '-Começo-'), sg.Button('Lap')],
+    [sg.VPush()]
+]
+
+Janela = sg.Window('Cronometro', 
+                   layout,
+                   size =(300,300),
+                   no_titlebar= True,
+                   element_justification = 'center')
 
 while True:
     event, values = Janela.read()
-    if event == sg.WIN_CLOSED:
+    if event in (sg.WIN_CLOSED, 'Começar'):
         break
     if event == '-Começo-':
        Janela.close()
