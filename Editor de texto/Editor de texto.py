18d9b5e113325dd5790c2bdd47f654982d246b1a
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-03-13 22:26:49 -0300

    Projeto 5 - Editor de texto Parte 2 -- adicionando elementos primários para o editor de texto

--- Editor de texto/Editor de texto.py
+++ Editor de texto/Editor de texto.py
@@ -1,7 +1,21 @@
 import PySimpleGUI as sg
 
+smileys = [
+   'feliz',[':)','xD',':D','<3'],
+   'triste',[':(','T_T'],
+   'outros',[':3']
+    ]
+
+menu_layout = [
+    ['Arquivo',['Abrir', 'Salvar','---','Sair']],
+    ['Mecânicas',['Contador de palavras']],
+    ['Adicionar',smileys]
+    ]
+
 layout = [
-    []
+    [sg.Menu(menu_layout)],
+    [sg.Text('Sem título', key='-documentoNome-')],
+    [sg.Multiline()]
 ]
 
 Janela = sg.Window('Editor de texto', layout)
