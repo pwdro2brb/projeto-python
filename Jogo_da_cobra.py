5e18c70fc811068e9c5f71bbc54de6950e3ea080
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-03-14 23:39:57 -0300

    Projeto 6 - Jogo da cobra Parte 2 -- Campo inicial do jogo no começo

--- Jogo_da_cobra.py
+++ Jogo_da_cobra.py
@@ -1,7 +1,17 @@
 import PySimpleGUI as sg
 
+#Tamanho do campo do jogo
+Tamanho_campo = 400
+
+sg.theme('LightGreen3')
+Campo = sg.Graph(
+    canvas_size= (Tamanho_campo,Tamanho_campo),
+    graph_bottom_left= (0,0),
+    graph_top_right= (Tamanho_campo,Tamanho_campo),
+    background_color = 'black'
+)
 layout = [
-    []
+    [Campo]
 ]
 
 Janela = sg.Window('Jogo da cobra', layout)
