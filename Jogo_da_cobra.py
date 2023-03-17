3a5aa4e6d2649b089c4022661a30e1d6a6da0300
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-03-17 11:07:44 -0300

    Projeto 6 - Jogo da cobra Parte 8 -- Movimentação da cobra responde as teclas e maçã some e reaparece após colisão

--- Jogo_da_cobra.py
+++ Jogo_da_cobra.py
@@ -1,10 +1,17 @@
 import PySimpleGUI as sg
 from time import time
+from random import randint
 
 def convert_pos_to_pixel(celula):
     tl = celula[0] * tamanho_celula, celula[1] * tamanho_celula
     br = tl[0] + tamanho_celula, tl[1] + tamanho_celula 
     return tl, br
+
+def colocar_maca():
+    maça_pos = randint(0,tamanho_celula - 1), randint(0,tamanho_celula-1)
+    while maça_pos in corpo_cobra:
+        maça_pos = randint(0, tamanho_celula - 1), randint(0, tamanho_celula - 1)
+    return maça_pos
 
 #Tamanho do campo do jogo
 Tamanho_campo = 400
@@ -17,7 +24,8 @@ DIRECTIONS = {'left': (-1,0), 'right': (1,0), 'up':(0,1), 'down':(0,-1)}
 direcao = DIRECTIONS['up']
 
 #Maçã
-maça_pos = (0,0)
+maça_pos = colocar_maca()
+comer_maca = False
 
 sg.theme('LightGreen3')
 Campo = sg.Graph(
@@ -37,26 +45,31 @@ start_time = time()
 while True:
     event, values = Janela.read(timeout=10)
     if event == sg.WIN_CLOSED: break
-    if event =='Left:37': print('Esquerda')
-    if event =='Up:38': print('Acima')
-    if event =='Right:39': print('Direita')
-    if event =='Down:40': print('Abaixo')
+    if event =='Left:37': direcao = DIRECTIONS['left']
+    if event =='Up:38': direcao = DIRECTIONS['up']
+    if event =='Right:39': direcao = DIRECTIONS['right']
+    if event =='Down:40': direcao = DIRECTIONS['down']
 
     #Tempo para aparecer outra maçã
     tempo_quando_começar = time() - start_time
-    if tempo_quando_começar >= 0.5:
+    if tempo_quando_começar >= 0.3:
         start_time = time()
+
+        #Colisão com a maçâ
+        if corpo_cobra[0] == maça_pos:
+            maça_pos = colocar_maca()
+            comer_maca = True
         
         #Atualiza a posição da cobra
         nova_cabeça = (corpo_cobra[0][0] + direcao[0],corpo_cobra[0][1] + direcao[1])
         corpo_cobra.insert(0, nova_cabeça)
         corpo_cobra.pop()
 
+        Campo.draw_rectangle((0,0),(Tamanho_campo,Tamanho_campo), 'black')
+
         #Desenha a maçã
         tl, br = convert_pos_to_pixel(maça_pos) 
         Campo.DrawRectangle(tl, br,'red')
-
-        Campo.draw_rectangle((0,0),(Tamanho_campo,Tamanho_campo), 'black')
 
         #Desenha a cobra
         for index, part in enumerate(corpo_cobra):
