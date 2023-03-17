78451bc41e41cc880608024d794b347076d7c5af
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-03-16 22:28:25 -0300

    Projeto 6 - Jogo da cobra Parte 5 -- Posicionamento da cobra.

--- Jogo_da_cobra.py
+++ Jogo_da_cobra.py
@@ -1,12 +1,21 @@
 import PySimpleGUI as sg
 
+def convert_pos_to_pixel(celula):
+    tl = celula[0] * tamanho_celula, celula[1] * tamanho_celula
+    br = tl[0] + tamanho_celula, tl[1] + tamanho_celula 
+    return tl, br
+
 #Tamanho do campo do jogo
 Tamanho_campo = 400
 numero_celula = 10
 tamanho_celula = Tamanho_campo/numero_celula
 
+#Cobra
+corpo_cobra = [(4,4),(3,4),(2,4)]
+DIRECTIONS = {'left': (-1,0), 'right': (1,0), 'up':(0,1), 'down':(0,-1)}
+
 #Maçã
-maça_pos = (4,5)
+maça_pos = (0,1)
 
 sg.theme('LightGreen3')
 Campo = sg.Graph(
@@ -29,8 +38,13 @@ while True:
     if event =='Right:39': print('Direita')
     if event =='Down:40': print('Abaixo')
 
-    tl = maça_pos[0] * tamanho_celula, maça_pos[1]* tamanho_celula
-    br = tl[0] + tamanho_celula, tl[1] + tamanho_celula
+    tl, br = convert_pos_to_pixel(maça_pos) 
     Campo.DrawRectangle(tl, br,'red')
+
+    #Desenha a cobra
+    for index, part in enumerate(corpo_cobra):
+        tl, br = convert_pos_to_pixel(part)
+        Cor = 'yellow' if index == 0 else 'green'
+        Campo.DrawRectangle(tl,br,Cor)
 
 Janela.close()
