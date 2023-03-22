0be74c565f049801b4d10423ced791312b19e52d
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-03-21 22:41:21 -0300

    Projeto 7 - Aplicativo de gráficos Parte 2 -- aplicando aparência básica do aplicativo

--- projetos 5, 6, 7  e  8/App de gráfico/grafico_App.py
+++ projetos 5, 6, 7  e  8/App de gráfico/grafico_App.py
@@ -1,7 +1,17 @@
 import PySimpleGUI as sg 
 
+sg.theme('LightGreen7')
+
+conteudo_tabela = []
+
 layout = [
-    []    
+    [sg.Table(
+    headings=['Observações', 'resultados'], 
+    values = conteudo_tabela, 
+    expand_x = True,
+    hide_vertical_scroll = True,
+    key='-Tabela-')],
+   [sg.Input(key='-input-', expand_x=True), sg.Button('Enviar')]    
 ]
 
 Janela = sg.Window('App de gráfico', layout)
@@ -10,5 +20,10 @@ while True:
     event, values = Janela.read()
     if event == sg.WIN_CLOSED:
         break
+
+    if event == 'Enviar':
+        novo_valor = values['-input-']
+        if novo_valor.isnumeric():
+            Janela['-Tabela-'].update([[1,10], [2,12], [3,0]])
 
 Janela.close()
