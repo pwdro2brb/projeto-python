a694030265c428e591e264673dc76252c40d59a0
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-03-22 23:10:55 -0300

    Projeto 8 - Editor de texto Parte 2 -- Estruta inicial do app

--- projetos 5, 6, 7  e  8/Editor de imagem/Editor_de_imagem.py
+++ projetos 5, 6, 7  e  8/Editor de imagem/Editor_de_imagem.py
@@ -1,10 +1,15 @@
 import PySimpleGUI as sg
 
-coluna_controle = True
+coluna_controle = sg.Column([
+    [sg.Slider(range = (0,10), orientation= 'h')],
+    [sg.Checkbox('Emboss', key='-Emboss-'), sg.Checkbox('Countour', key='-Countor-')],
+    [sg.Checkbox('Virar x', key='-VirarX-'), sg.Checkbox('Virar y', key='-VirarY-')],
+    [sg.Button('Salvar imagem', key='-Salvar-')],
+])
 coluna_imagem = sg.Column([[sg.Image('projetos 5, 6, 7  e  8\Editor de imagem\cross.jpg')]])
 
 layout = [
-    [coluna_imagem]
+    [coluna_controle, coluna_imagem]
 ]
 
 Janela =  sg.Window('Editor de imagem', layout)
