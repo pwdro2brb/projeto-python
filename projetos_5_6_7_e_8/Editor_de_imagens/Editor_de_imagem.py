fc373586080c4804486fa3f72cac183f6ab3f18c
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-03-27 22:33:15 -0300

    Projeto 8 - editor de imagem Parte 4 -- Funciona a edição de imagem completa (erro más funciona)

--- projetos_5_6_7_e_8/Editor_de_imagens/Editor_de_imagem.py
+++ projetos_5_6_7_e_8/Editor_de_imagens/Editor_de_imagem.py
@@ -1,10 +1,20 @@
 import PySimpleGUI as sg
-from PIL import Image, ImageFilter
+from PIL import Image, ImageFilter, ImageOps
 from io import BytesIO
 
 def atualiza_imagem(original, blur, contraste, Emboss, Countor, flipx, flipy, Janela):
     image = original.filter(ImageFilter.GaussianBlur(blur))
+    image = image.filter(ImageFilter.UnsharpMask(contraste))
     
+    if Emboss:
+        image = image.filter(ImageFilter.EMBOSS())
+    if Countor:
+        image = image.filter(ImageFilter.CONTOUR())
+    if flipx:
+        image = ImageOps.mirror(image)
+    if flipy:
+        image = ImageOps.flip(image)   
+         
     bio = BytesIO()
     image.save(bio, format = 'PNG')
     
@@ -15,7 +25,7 @@ caminho_imagem = 'projetos_5_6_7_e_8\Editor_de_imagens\imagem.PNG'
 coluna_controle = sg.Column([
     [sg.Frame('Borrado', layout = [[sg.Slider(range = (0,10), orientation= 'h', key='-blur-')]])],
     [sg.Frame('Contraste', layout = [[sg.Slider(range = (0,10), orientation= 'h', key='-Contraste-')]])],
-    [sg.Checkbox('Emboss', key='-Emboss-'), sg.Checkbox('Countour', key='-Countor-')],
+    [sg.Checkbox('Relevo', key='-Emboss-'), sg.Checkbox('Contorno', key='-Countor-')],
     [sg.Checkbox('Virar x', key='-VirarX-'), sg.Checkbox('Virar y', key='-VirarY-')],
     [sg.Button('Salvar imagem', key='-Salvar-')],
 ])
