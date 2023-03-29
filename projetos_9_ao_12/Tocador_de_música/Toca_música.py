00362b19ac2cf7c77e739c06c72082ac21b740e2
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-03-28 23:12:08 -0300

    Projeto 9 - Tocador de música Parte 4 -- Finalizando botões de pause, play e volume Aparece mensagem de erro mas funciona

--- projetos_9_ao_12/Tocador_de_música/Toca_música.py
+++ projetos_9_ao_12/Tocador_de_música/Toca_música.py
@@ -14,21 +14,28 @@ def base64_image_import(path):
 sg.theme('reddit')
 
 play_layout = [
-    [sg.Text('Nome do som')],
+    [sg.VPush()],
+    [sg.Push(),sg.Text('Nome do som', font = 'Arial 20'),sg.Push],
+    [sg.VPush()],
     [
-        sg.Button(image_data = base64_image_import('projetos_9_ao_12\Tocador_de_música\Play.png')),
-        sg.Button(image_data = base64_image_import('projetos_9_ao_12\Tocador_de_música\Pausar.png')),
+        sg.Push(),
+        sg.Button(image_data = base64_image_import('projetos_9_ao_12\Tocador_de_música\Play.png'), button_color = 'white',border_width = 0),
+        sg.Text(' '),
+        sg.Button(image_data = base64_image_import('projetos_9_ao_12\Tocador_de_música\Pausar.png'), button_color = 'white',border_width = 0),
+        sg.Push(),
     ],
+    [sg.VPush()],
+    [sg.Progress(100, size=(50,50))],
 ]
 
-layout_volume = [
+volume_layout = [
     [sg.VPush()],
     [sg.Push(),sg.Slider(range = (0,100), default_value = 100, orientation='h', key='Volume'),sg.Push()],
     [sg.VPush()],
     ]
 
 layout = [
-    [sg.TabGroup([[sg.Tab('Play',play_layout),sg.Tab('Volume',layout_volume)]])],
+    [sg.TabGroup([[sg.Tab('Play',play_layout),sg.Tab('Volume',volume_layout)]])],
 ]
 
 Janela = sg.Window('toca_música', layout)
