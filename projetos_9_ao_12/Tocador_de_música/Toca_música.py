520d6e51e78d51524c75b93f62e677ed20584bb7
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-03-28 22:45:48 -0300

    Projeto 9 - Tocador de música Parte 3 -- Adicionando botão de pause e play, também adicionau volume

--- projetos_9_ao_12/Tocador_de_música/Toca_música.py
+++ projetos_9_ao_12/Tocador_de_música/Toca_música.py
@@ -1,20 +1,37 @@
 import PySimpleGUI as sg
 
+import base64
+from io import BytesIO
+from PIL import Image
+
+def base64_image_import(path):
+    image = Image.open(path)
+    buffer = BytesIO()
+    image.save(buffer, format='PNG')
+    b64 = base64.b64encode(buffer.getvalue())
+    return b64
+
 sg.theme('reddit')
 
 play_layout = [
-    [sg.Text('Nome do som')]
+    [sg.Text('Nome do som')],
+    [
+        sg.Button(image_data = base64_image_import('projetos_9_ao_12\Tocador_de_música\Play.png')),
+        sg.Button(image_data = base64_image_import('projetos_9_ao_12\Tocador_de_música\Pausar.png')),
+    ],
 ]
 
 layout_volume = [
-    [sg.Slider(range = (0,100))]
+    [sg.VPush()],
+    [sg.Push(),sg.Slider(range = (0,100), default_value = 100, orientation='h', key='Volume'),sg.Push()],
+    [sg.VPush()],
     ]
 
 layout = [
     [sg.TabGroup([[sg.Tab('Play',play_layout),sg.Tab('Volume',layout_volume)]])],
 ]
 
-Janela = sg.Window('Tocador de música', layout)
+Janela = sg.Window('toca_música', layout)
 
 while True:
     event, values = Janela.read()
