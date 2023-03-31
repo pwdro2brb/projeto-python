cb9c7f8ffc42ec60cab7431e2af4079fb665bc44
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-03-30 22:25:57 -0300

    Projeto 10 - App para olhar o clima de países Parte 2 -- Estrutura inicial do app

--- projetos_9_ao_12/App_que_olha_clima/App_para_clima.py
+++ projetos_9_ao_12/App_que_olha_clima/App_para_clima.py
@@ -1,7 +1,15 @@
 import PySimpleGUI as sg
 
+Coluna_imagem = sg.Column([sg.Image(key = '-Imagem-', background_color='FFFFFF')])
+coluna_info = sg.Column([[
+    [sg.Text('', key = '-Localizacao-',font = 'Calibri 30', background_color = '#FF0000', pad = 0, visible = False)],
+    [sg.Text('', key = '-Tempo-',font = 'Calibri 16', background_color = '#000000', pad = 0, visible = False, text_color='#FFFFFF')],
+    [sg.Text('', key = '-Temp-',font = 'Calibri 16', pad = (0,10), background_color = '#FFFFFF', visible = False, text_color='#000000', justification = 'center')],
+]])
+
 layout = [
-    [sg.Button('Começando um novo app', key='b')]
+    [sg.Input(expand_x = True, key = '-Input-'),sg.Button('Entrar')],
+    [Coluna_imagem, coluna_info]
 ]
 
 Janela = sg.Window('App_para_clima', layout)
@@ -10,7 +18,6 @@ while True:
     event, values =  Janela.read()
     if event == sg.WIN_CLOSED:
         break
-    if event == 'b':
-        Janela.close()
+
 
 Janela.close()
