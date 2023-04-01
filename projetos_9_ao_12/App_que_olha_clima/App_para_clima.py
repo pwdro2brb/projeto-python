82f0501f89dd75dc7867d8b1626f670f6a4479c3
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-03-31 22:56:42 -0300

    Projeto 10 - App para olhar o clima de países Parte 3 -- Layout básico pronto

--- projetos_9_ao_12/App_que_olha_clima/App_para_clima.py
+++ projetos_9_ao_12/App_que_olha_clima/App_para_clima.py
@@ -1,14 +1,15 @@
 import PySimpleGUI as sg
+sg.theme('reddit')
 
-Coluna_imagem = sg.Column([sg.Image(key = '-Imagem-', background_color='FFFFFF')])
-coluna_info = sg.Column([[
-    [sg.Text('', key = '-Localizacao-',font = 'Calibri 30', background_color = '#FF0000', pad = 0, visible = False)],
-    [sg.Text('', key = '-Tempo-',font = 'Calibri 16', background_color = '#000000', pad = 0, visible = False, text_color='#FFFFFF')],
-    [sg.Text('', key = '-Temp-',font = 'Calibri 16', pad = (0,10), background_color = '#FFFFFF', visible = False, text_color='#000000', justification = 'center')],
-]])
+Coluna_imagem = sg.Column([[sg.Image(key = '-Imagem-', background_color='#FFFFFF')]])
+coluna_info = sg.Column([
+    [sg.Text('', key = '-Localizacao-', font = 'Calibri 30', background_color = '#FF0000', text_color='#FFFFFf', pad = 0, visible = False)],
+    [sg.Text('', key = '-Tempo-', font = 'Calibri 16', background_color = '#000000', pad = 0, visible = False, text_color='#FFFFFF')],
+    [sg.Text('', key = '-Temp-', font = 'Calibri 16', pad = (0,10), background_color = '#FFFFFF', visible = False, text_color='#000000', justification = 'center')],
+])
 
 layout = [
-    [sg.Input(expand_x = True, key = '-Input-'),sg.Button('Entrar')],
+    [sg.Input(expand_x = True, key = '-Input-'),sg.Button('Entrar', button_color='#000000', border_width=0)],
     [Coluna_imagem, coluna_info]
 ]
 
@@ -18,6 +19,11 @@ while True:
     event, values =  Janela.read()
     if event == sg.WIN_CLOSED:
         break
-
+    
+    if event == 'Entrar':
+        Janela['-Localizacao-'].update('test', visible = True)
+        Janela['-Tempo-'].update('test', visible = True)
+        Janela['-Temp-'].update('test', visible = True)
+        Janela['-Imagem-'].update('projetos_9_ao_12\App_que_olha_clima\Imagens\Gelado.png')
 
 Janela.close()
