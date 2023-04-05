db2855cf0389d7121f6500cd5247a63f22cbed38
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-04-04 23:52:29 -0300

    Projeto 11 - App para entrada de dados para o excel Parte 4 -- Botão para apagar os dados

--- projetos_9_ao_12/Formulário_para_entrada_de_dados _excel/App_para_excel.py
+++ projetos_9_ao_12/Formulário_para_entrada_de_dados _excel/App_para_excel.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 sg.theme('DarkTeal4')
 
-EXCEL_FILE='projetos_9_ao_12\Formulário_para_entrada_de_dados _excel\Testes_para_codigo.xlsx', 'projetos_9_ao_12\Formulário_para_entrada_de_dados _excel\~$Testes_para_codigo.xlsx'
+EXCEL_FILE='projetos_9_ao_12\Formulário_para_entrada_de_dados _excel\Testes_para_codigo.xlsx'
 df = pd.read_excel(EXCEL_FILE)
 
 layout = [
@@ -21,20 +21,28 @@ layout = [
     [sg.Text('Número de crianças', size=(15,1)), sg.Spin([i for i in range(0,16)],
                                                          initial_value=0, key='-CRIANÇA-')],
  
-    [sg.Submit('Enviar', key='Submit'),sg.Exit('Sair', key='sair')],
+    [sg.Submit('Enviar', key='Submit'), sg.Button('Limpar'),sg.Exit('Sair', key='sair')],
 ]
 
 Janela = sg.Window('App para receber dados daqui e enviar para o excel', layout)
 
+def limpar_input():
+    for key in values:
+        Janela[key]('')
+    return None
+        
 
 while True:
     event, values = Janela.read()
     if event == sg.WIN_CLOSED:
         break
+    if event == 'Limpar':
+        limpar_input()
     if event == 'Submit':
         df = df._append(values, ignore_index=True)
         df.to_excel(EXCEL_FILE, index=False)
         sg.popup('Dados salvados!!!')
+        limpar_input()
 
 Janela.close()
 
