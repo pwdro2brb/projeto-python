980bdcfa1fddb2ce7f9764c6cf73d084e585e752
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-04-04 23:16:58 -0300

    Projeto 11 - App para entrada de dados para o excel Parte 2 -- layout completado

--- projetos_9_ao_12/Formulário_para_entrada_de_dados _excel/App_para_excel.py
+++ projetos_9_ao_12/Formulário_para_entrada_de_dados _excel/App_para_excel.py
@@ -1,11 +1,23 @@
 import PySimpleGUI as sg
+import pandas as pd
 
 sg.theme('DarkTeal4')
 
 layout = [
     [sg.Text('Por favor preencha os seguintes campos: ')],
     [sg.Text('Nome', size=(15,1)), sg.InputText(key='Nome')],
-    [sg.Submit('Enviar', key='Submit'),sg.Exit('Sair', key='sair')]
+    [sg.Text('Cor favorita', size=(15,1)), sg.Combo(['Verde', 'Azul', 'Vermelho'], key='Cor Favorita')],
+    [sg.Text('Eu falo', size=(15,1)),
+            sg.Checkbox('Alemão', key='Alemão'),
+            sg.Checkbox('Português', key='Brasileiro'),
+            sg.Checkbox('Espanhou', key='Espanhou'),
+            sg.Checkbox('Inglês', key='Inglês'),
+            sg.Checkbox('Mandarin', key='Mandarin'),
+        ],
+    [sg.Text('Número de crianças', size=(15,1)), sg.Spin([i for i in range(0,16)],
+                                                         initial_value=0, key='-CRIANÇA-')],
+ 
+    [sg.Submit('Enviar', key='Submit'),sg.Exit('Sair', key='sair')],
 ]
 
 Janela = sg.Window('App para receber dados daqui e enviar para o excel', layout)
