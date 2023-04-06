20064dc6751e3575c2bd60b35615d6c2f1d4acd6
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-04-05 23:21:48 -0300

    Projeto 12 - App para Gerar senha Parte 3 -- Layout do app terminado

--- projetos_9_ao_12/Gerador_de_senha/Gerador_de_senha.py
+++ projetos_9_ao_12/Gerador_de_senha/Gerador_de_senha.py
@@ -1,5 +1,6 @@
 import random
 import string
+import PySimpleGUI as sg
 
 Maiúsculo = random.sample(string.ascii_uppercase, 2)
 Minúsculo = random.sample(string.ascii_lowercase, 2)
@@ -11,10 +12,17 @@ total = random.sample(total, len(total))
 total = ''.join(total)
 print(total)
 
-import PySimpleGUI as sg
+sg.theme('LightBlue2')
+sg.set_options(font='Verdana 15')
 
 layout = [
-    [sg.Text('Começando a codificação                                                      ')]
+    [sg.Text('Crie senhas com o app, escolhas quantos carcteres de cada tipo abaixo terá na senha que será criada')],
+    [sg.Text('Quantidade Maiúsculas'), sg.Input(size = 15)],
+    [sg.Text('Quantidade Minúsculas'), sg.Input(size = 15)],
+    [sg.Text('Quantidade Números   '), sg.Input(size = 15)],
+    [sg.Text('Quantidade Simbolos   '), sg.Input(size = 15)],
+    [sg.Button('Ok'), sg.Button('Cancelar')],
+    [sg.Text('Senha'), sg.Multiline(size = 15, no_scrollbar=True, disabled=True)]
 ]
 
 janela = sg.Window('Gerador de senha', layout)
