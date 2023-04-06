3d2146bc71df60e0e37079b20729ced4d445bd49
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-04-05 23:34:03 -0300

    Projeto 12 - App para Gerar senha Parte 4 -- Tamanho da senha é alterado de acordo com o usuário

--- projetos_9_ao_12/Gerador_de_senha/Gerador_de_senha.py
+++ projetos_9_ao_12/Gerador_de_senha/Gerador_de_senha.py
@@ -2,33 +2,38 @@ import random
 import string
 import PySimpleGUI as sg
 
-Maiúsculo = random.sample(string.ascii_uppercase, 2)
-Minúsculo = random.sample(string.ascii_lowercase, 2)
-digitos = random.sample(string.digits, 2)
-simbolos = random.sample(string.punctuation, 2)
-
-total = Maiúsculo+Minúsculo+digitos+simbolos
-total = random.sample(total, len(total))
-total = ''.join(total)
-print(total)
 
 sg.theme('LightBlue2')
 sg.set_options(font='Verdana 15')
 
 layout = [
     [sg.Text('Crie senhas com o app, escolhas quantos carcteres de cada tipo abaixo terá na senha que será criada')],
-    [sg.Text('Quantidade Maiúsculas'), sg.Input(size = 15)],
-    [sg.Text('Quantidade Minúsculas'), sg.Input(size = 15)],
-    [sg.Text('Quantidade Números   '), sg.Input(size = 15)],
-    [sg.Text('Quantidade Simbolos   '), sg.Input(size = 15)],
+    [sg.Text('Quantidade Maiúsculas'), sg.Input(size = 15, key='-Maiúsculo-')],
+    [sg.Text('Quantidade Minúsculas'), sg.Input(size = 15, key='-Minúsculo-')],
+    [sg.Text('Quantidade Números   '), sg.Input(size = 15, key='-Digito-')],
+    [sg.Text('Quantidade Simbolos   '), sg.Input(size = 15, key='-Simbolo-' )],
     [sg.Button('Ok'), sg.Button('Cancelar')],
-    [sg.Text('Senha'), sg.Multiline(size = 15, no_scrollbar=True, disabled=True)]
+    [sg.Text('Senha'), sg.Multiline(size = 15, no_scrollbar=True, disabled=True, key='-Passar-')]
 ]
 
 janela = sg.Window('Gerador de senha', layout)
 
 while True:
-    events, values = janela.read()
-    if events == sg.WIN_CLOSED:
+    event, values = janela.read()
+    if event == sg.WIN_CLOSED or event == 'Cancelar':
         break
+    if event == 'Ok':       
+        voce_maior = int(values['-Maiúsculo-'])
+        Maiúsculo = random.sample(string.ascii_uppercase, voce_maior)
+        voce_menor = int(values['-Minúsculo-'])
+        Minúsculo = random.sample(string.ascii_lowercase, voce_menor)
+        voce_núm = int(values['-Digito-'])
+        digitos = random.sample(string.digits, voce_núm)
+        voce_simb = int(values['-Simbolo-'])
+        simbolos = random.sample(string.punctuation, voce_simb)
+
+        total = Maiúsculo+Minúsculo+digitos+simbolos
+        total = random.sample(total, len(total))
+        total = ''.join(total)
+        print(total)
 janela.close()
