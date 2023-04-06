e20f6523715c6136d930f31ff85c86a40ee81ae2
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-04-05 23:06:31 -0300

    Projeto 12 - App para Gerar senha Parte 2 -- Script da criação de senha

--- projetos_9_ao_12/Gerador_de_senha/Gerador_de_senha.py
+++ projetos_9_ao_12/Gerador_de_senha/Gerador_de_senha.py
@@ -1,3 +1,16 @@
+import random
+import string
+
+Maiúsculo = random.sample(string.ascii_uppercase, 2)
+Minúsculo = random.sample(string.ascii_lowercase, 2)
+digitos = random.sample(string.digits, 2)
+simbolos = random.sample(string.punctuation, 2)
+
+total = Maiúsculo+Minúsculo+digitos+simbolos
+total = random.sample(total, len(total))
+total = ''.join(total)
+print(total)
+
 import PySimpleGUI as sg
 
 layout = [
